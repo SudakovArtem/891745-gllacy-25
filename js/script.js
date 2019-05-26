e8d0b8cf4943975fb51b14463505aa18453f80ce
Artem <50323141+SudakovArtem@users.noreply.github.com>
2019-05-26 19:39:14 +0300

    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ var close = feedback.querySelector(".feedback-popup__close");
 var form = feedback.querySelector("form");
 var name = feedback.querySelector("[name=login]");
 var email = feedback.querySelector("[name=email]");
+var slideOne = document.querySelector(".promo__radio--one");
+var slideTwo = document.querySelector(".promo__radio--two");
+var slideThree = document.querySelector(".promo__radio--three");
+
+slideTwo.addEventListener("click", function () {
+    document.body.classList.add("slide-two");
+    document.body.classList.remove("slide-three");
+
+});
+
+slideThree.addEventListener("click", function () {
+    document.body.classList.remove("slide-two");
+    document.body.classList.add("slide-three");
+});
+
+slideOne.addEventListener("click", function () {
+    document.body.classList.remove("slide-two");
+    document.body.classList.remove("slide-three");
+});
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
